1cf2156fadbe53cfced673f3d3b0ca2e80389011
TheoKratz <theo.kratz.t@gmail.com>
2020-08-26 21:01:08 +0200

    la consulta envia datos a la base de datos

--- AmazonDataApp/views.py
+++ AmazonDataApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from AmazonDataApp.scrapping import get_data
+from AmazonDataApp.models import DataSheet
 
 # Create your views here.
 def busqueda_asin(request):
@@ -13,8 +14,10 @@ def resultado_busqueda(request):
     producto = request.GET["prd"]
     if producto:
 
-            list=get_data(producto)
-            return render(request,"AmazonDataApp/resultados.html",{"asin":producto, "lista":list})
+            lista=get_data(producto)
+            as1=DataSheet(asin=producto, titulo=lista[0],bullet_1=lista[1],bullet_2=lista[2],bullet_3=lista[3],bullet_4=lista[4],bullet_5=lista[5])
+            as1.save()
+            return render(request,"AmazonDataApp/resultados.html",{"asin":producto, "lista":lista})
 
     else:
         mensaje= "No se han encontrado productos"
