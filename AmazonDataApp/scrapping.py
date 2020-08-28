5eb67362be933713b1908ceb5554a6affcdad64f
TheoKratz <theo.kratz.t@gmail.com>
2020-08-28 20:27:39 +0200

    ya funciona todo, ahora estilos

--- AmazonDataApp/scrapping.py
+++ AmazonDataApp/scrapping.py
@@ -3,17 +3,12 @@ import requests
 from AmazonDataApp.models import DataSheet
 from django.db import models
 
-
-
-
 def get_data(asin):
     headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
-}
-
+    }
     url = "https://www.amazon.es/dp/"+asin
     data_list=[]
-
     response = requests.post(url,headers=headers)
 
     if response.status_code == 200:
@@ -21,29 +16,27 @@ def get_data(asin):
         soup = BeautifulSoup(texto,"lxml")
         respuesta = soup.find(class_="a-unordered-list a-vertical a-spacing-mini").find_all(class_="a-list-item")
         data_list.append(soup.title.text)
-    for item in respuesta:
-        data_list.append(item.text)
-        
+
+        for item in respuesta:
+            data_list.append(item.text)        
     return data_list
 
-def compare_data(asin,lista):
 
+def compare_data(asin,lista):
     try:
         comp = DataSheet.objects.filter(asin=asin).order_by("-query_date")
-    except:
-        pass
-    if comp:
         if (lista[0]==comp[0].titulo and lista[1]==comp[0].bullet_1 and lista[2]==comp[0].bullet_2 and lista[3]==comp[0].bullet_3 and lista[4]==comp[0].bullet_4 and lista[5]==comp[0].bullet_5):
             pass            
         else:
             as1=DataSheet(asin=asin, titulo=lista[0],bullet_1=lista[1],bullet_2=lista[2],bullet_3=lista[3],bullet_4=lista[4],bullet_5=lista[5])
             as1.save()
-        
-    else:
-        as1=DataSheet(asin=asin, titulo=lista[0],bullet_1=lista[1],bullet_2=lista[2],bullet_3=lista[3],bullet_4=lista[4],bullet_5=lista[5])
-        as1.save()
-        comp=["no hay registros previos"]
-    
+    except:
+        try:
+            as1=DataSheet(asin=asin, titulo=lista[0],bullet_1=lista[1],bullet_2=lista[2],bullet_3=lista[3],bullet_4=lista[4],bullet_5=lista[5])
+            as1.save()
+            comp=["no hay registros previos"]
+        except:
+            comp =["No es un asin valido"]    
     return comp
     
 
--- AmazonDataApp/views.py
+++ AmazonDataApp/views.py
@@ -5,24 +5,16 @@ from AmazonDataApp.models import DataSheet
 
 # Create your views here.
 def busqueda_asin(request):
-    
 
     return render(request,"AmazonDataApp/base.html")
 
 
 def resultado_busqueda(request):
     producto = request.GET["prd"]
-    if producto:
-        lista=get_data(producto)
-        comp = compare_data(producto,lista)
+    lista=[]
+    lista=get_data(producto)
+    comp = compare_data(producto,lista)
+    return render(request,"AmazonDataApp/resultados.html",{"asin":producto, "lista":lista,"comp":comp})
 
-        return render(request,"AmazonDataApp/resultados.html",{"asin":producto, "lista":lista,"comp":comp})
-
-
-
-    else:
-        mensaje= "No se han encontrado productos"
-    
-    return HttpResponse(mensaje)
 
 
